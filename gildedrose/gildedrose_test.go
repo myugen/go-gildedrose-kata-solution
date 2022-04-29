3dd098decb0551c2f4908944b58bd4ed27d8f1bf
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2022-04-30 00:39:59 +0100

    :bug: fix: fix update quality test

--- gildedrose/gildedrose_test.go
+++ gildedrose/gildedrose_test.go
@@ -4,16 +4,34 @@ import (
 	"testing"
 
 	"github.com/myugen/go-gildedrose-solution/gildedrose"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestUpdateQuality(t *testing.T) {
-	var items = []*gildedrose.Item{
-		{"foo", 0, 0},
+	testcases := map[string]struct {
+		item *gildedrose.Item
+		want *gildedrose.Item
+	}{
+		"+5 Dexterity Vest Item": {
+			item: &gildedrose.Item{
+				Name:    "+5 Dexterity Vest",
+				SellIn:  10,
+				Quality: 20,
+			},
+			want: &gildedrose.Item{
+				Name:    "+5 Dexterity Vest",
+				SellIn:  9,
+				Quality: 19,
+			},
+		},
 	}
 
-	gildedrose.UpdateQuality(items)
-
-	if items[0].Name != "fixme" {
-		t.Errorf("Name: Expected %s but got %s ", "fixme", items[0].Name)
+	for name, testcase := range testcases {
+		t.Run(name, func(t *testing.T) {
+			items := []*gildedrose.Item{testcase.item}
+			gildedrose.UpdateQuality(items)
+			assert.Equal(t, testcase.want.SellIn, testcase.item.SellIn)
+			assert.Equal(t, testcase.want.Quality, testcase.item.Quality)
+		})
 	}
 }
