6db29f89d4621f229a8216d6e5839e41b9c6dc46
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2022-05-09 21:42:38 +0100

    :recycle: refactor: move item to an independent file

--- gildedrose/gildedrose.go
+++ gildedrose/gildedrose.go
@@ -1,10 +1,5 @@
 package gildedrose
 
-type Item struct {
-	Name            string
-	SellIn, Quality int
-}
-
 func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
 
---
+++ gildedrose/item.go
@@ -0,0 +1,6 @@
+package gildedrose
+
+type Item struct {
+	Name            string
+	SellIn, Quality int
+}
